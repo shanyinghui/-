2d5cc3f9e8a7db1fd979409ace230c410ab1adcd
853196871 <853196871@qq.com>
2019-12-13 15:41:59 +0800

    .

--- src/main/java/com/buba/stuinfomanager/mapper/SummaryMapper.java
+++ src/main/java/com/buba/stuinfomanager/mapper/SummaryMapper.java
@@ -3,7 +3,6 @@ package com.buba.stuinfomanager.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
 
-@Mapper
 @Component
 public interface SummaryMapper {
 
